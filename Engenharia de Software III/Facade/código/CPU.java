addb86110317abc2f68670b94d6898ee72538d03
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2024-05-27 09:34:31 -0300

    atualização correção

---
+++ Engenharia de Software III/Facade/código/CPU.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package facedepattern;
+
+/**
+ *
+ * @author guara
+ */
+public class CPU {
+    public void iniciar(){
+        System.out.println("Log: 'CPU iniciada!'");
+    }    
+}
